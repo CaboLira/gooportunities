f98c478b57716b6f7e521cff4b735eb3d5c0c5ad
CaboLira <lucasdunlopfernandescoachman@gmail.com>
2024-02-29 12:49:05 -0300

    wip

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/CaboLira/gooportunities.git/config"
 	"github.com/CaboLira/gooportunities.git/router"
 )
 
 func main() {
+
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Initialize Router
 	router.Inicialize()
 }
